e74d1638fc88b713d032a4e7e76b32f05bd16818
王志强 <306621759@qq.com>
2022-03-11 15:56:32 +0800

    测试

---
+++ src/Plugin.php
@@ -0,0 +1,20 @@
+<?php
+namespace Jisheng100;
+
+class Plugin
+{
+    public function getAll()
+    {
+        return [];
+    }
+
+    public function get()
+    {
+
+    }
+
+    public function test($msg)
+    {
+        return $msg;
+    }
+}
